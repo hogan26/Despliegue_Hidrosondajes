f1938eee6da3b6e9dcfb13cda0b8c10440c58e76
Hogan26 <nelson.gonzalezguerra@gmail.com>
2021-09-02 21:56:18 +0000

    prueba7

--- req_ventas/models/.ipynb_checkpoints/utility_lines-checkpoint.py
+++ req_ventas/models/.ipynb_checkpoints/utility_lines-checkpoint.py
@@ -42,21 +42,16 @@ class SaleOrderLine(models.Model):
                 'utilidad_porcentaje')
     def _compute_amount(self):
         for line in self:
-            price = (line.price_unit + line.utilidad_unitaria) * (
-            1- (line.discount or 0.0)/100.0)
-            taxes = line.tax_id.compute_all(price,line.order_id.currency_id,
-                                   line.product_uom_qty,
-                                   product=line.product_id,
-                                   partner=line.order_id.partner_shipping_id)
+            price = (line.price_unit + line.utilidad_unitaria) * (1- (line.discount or 0.0)/100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
             line.update({
-                'price.tax':sum(
-                t.get('amount',0.0) for t in taxes.get('taxes',[])),
-                'price_total':taxes['total_included'],
-                'price_subtotal':taxes['total_excluded'],
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
             })
-            if self.env.context.get('import_file',False) and not self.env.user.user_has_groups('account.group_account_manager'):
-                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'],[line.tax_id.id])
-    
+            if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
+                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])                
+                
     utilidad_porcentaje = fields.Float(string="Utilidad (%)", default=45)
     utilidad_unitaria = fields.Monetary(string="U. unitaria ($)",
                                        compute=utilidad_unitaria)
