127a0059a9389b719756a9a120ea07197a80aeee
Hogan26 <nelson.gonzalezguerra@gmail.com>
2021-11-04 19:29:08 +0000

    probando campos para ultima actualizacion de precio de producto

---
+++ req_inventario/models/last_update_pricelist_date.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class req_inventario(models.Model):
+    _inherit='product.template'
+    
+    last_update_pricelist_date = fields.Date(string='Ultima actulizacion de precio')
+    last_update_pricelist_partner = fields.Many2one('res.partner',string='Proveedor',readonly=True)
+
