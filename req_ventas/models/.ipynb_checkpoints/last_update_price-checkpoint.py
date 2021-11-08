763374c2ce8f55c8fae3bae462aa2fd901164530
Hogan26 <nelson.gonzalezguerra@gmail.com>
2021-11-08 15:49:48 +0000

    prueba 3

--- req_ventas/models/.ipynb_checkpoints/last_update_price-checkpoint.py
+++ req_ventas/models/.ipynb_checkpoints/last_update_price-checkpoint.py
@@ -113,4 +113,4 @@ class LastUpdatePriceLine(models.Model):
     _inherit='sale.order.line'
     
     last_update_price_date = fields.Date('product_template_id.last_update_price_date',readonly=True)
-    last_update_price_partner = fields.Many2One('product.template','product_template_id.last_update_price_partner',readonly=True)
+    last_update_price_partner = fields.Char('product_template_id.last_update_price_partner',readonly=True)
