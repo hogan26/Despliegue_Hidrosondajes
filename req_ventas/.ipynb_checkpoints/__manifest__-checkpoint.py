9c2c8a8e21d0e283cd6ea0e0f89ad5005bc05cb2
Hogan26 <nelson.gonzalezguerra@gmail.com>
2021-11-08 14:32:03 +0000

    probando generacion automatica de cotizaciones y campo de ultima actualizacion de precio en presupuestos

--- req_ventas/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ req_ventas/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,33 +3,28 @@
     'name': "req_ventas",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        sale hidrosondajes requeriments""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Hidrosondajes",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sale',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+        'views/utility_lines_view.xml',
+        'views/last_update_price_partner.xml',
+    ],    
 }
--- req_ventas/models/__init__.py
+++ req_ventas/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import utility_lines
+from . import utility_lines,last_update_price
