615107c516e59de0bd0e58ffb8388398f03a25ad
Hogan26 <nelson.gonzalezguerra@gmail.com>
2022-01-11 20:35:56 +0000

    descuento al iva total

--- req_ventas/models/__init__.py
+++ req_ventas/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import utility_lines,last_update_price,fields_crm_to_sale,ot_fields
+from . import utility_lines,last_update_price,fields_crm_to_sale,ot_fields,total_tax_discount
---
+++ req_ventas/models/total_tax_discount.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from datetime import timedelta
+from odoo import models, fields, api
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(models.Model):
+    _inherit='sale.order'
+    
+    @api.depends('order_line.price_total','total_tax_discount')
+    def _amount_all(self):
+        """
+        Compute the total amounts of the SO.
+        """
+        for order in self:
+            amount_untaxed = amount_tax = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+            order.update({
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax - (amount_tax*(order.total_tax_discount/100)),
+                'amount_total': amount_untaxed + amount_tax,
+            })
+    
+    total_tax_discount = fields.Integer(string="Descuento (%)",default=0)
+    
+    
